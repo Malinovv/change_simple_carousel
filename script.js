2cb037595e7f3eea22bccb74e0a71f470964fe0d
Kristijan Malinov <kristijan.malinovv@gmail.com>
2023-11-24 16:45:34 +0100

    carousel finished

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// my solution
+
 let currentSlide = 0;
 const totalSlides = document.querySelectorAll(".slide").length;
 const carousel = document.querySelector(".carousel");
@@ -8,12 +10,42 @@ nextBtn.addEventListener("click", () => changeSlide(1));
 prevBtn.addEventListener("click", () => changeSlide(-1));
 
 function showSlide(index) {
-  // recalculate the currentSlide
-  // move the carousel to the currentSlide
+  if (index < 0) {
+    index = totalSlides - 1;
+  } else if (index >= totalSlides) {
+    index = 0;
+  }
+
+  currentSlide = index;
+
+  const translateValue = -currentSlide * 100 + "%";
+  carousel.style.transform = "translateX(" + translateValue + ")";
+  carousel.style.transition = "0.5s";
 }
 
 function changeSlide(direction) {
-  // get the currentSlide value and add the direction to it
-  // assign that to a local variable called index
-  // call showSlide with index
+  const index = currentSlide + direction;
+  showSlide(index);
 }
+
+//* Viktor's example:
+
+// let currentSlide = 0;
+// const totalSlides = document.querySelectorAll(".slide").length;
+// const carousel = document.querySelector(".carousel");
+// const nextBtn = document.querySelector("#nextBtn");
+// const prevBtn = document.querySelector("#prevBtn");
+
+// nextBtn.addEventListener("click", () => changeSlide(1));
+// prevBtn.addEventListener("click", () => changeSlide(-1));
+
+// function showSlide(index) {
+//   // recalculate the currentSlide
+//   // move the carousel to the currentSlide
+// }
+
+// function changeSlide(direction) {
+//   // get the currentSlide value and add the direction to it
+//   // assign that to a local variable called index
+//   // call showSlide with index
+// }
